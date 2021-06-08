1250da43920f4a93bb143f466ea57de759f4dc11
HongJunHyeok <edb1631@naver.com>
2021-06-08 17:14:32 +0900

    ADD swapping items

--- Sortable_List/script.js
+++ Sortable_List/script.js
@@ -10,6 +10,7 @@ const list = [
   "넷플릭스",
   "TV",
   "플레이 스테이션",
+  "리그 오브 레전드",
 ];
 
 const listItems = [];
@@ -37,8 +38,56 @@ function createList() {
         </div>
       `;
 
-      listItems.push = listItem;
+      listItems.push(listItem);
 
       draggable_list.appendChild(listItem);
     });
+  addEventListeners();
+}
+
+function dragStart() {
+  dragStartIndex = +this.closest("li").getAttribute("data-index");
+}
+
+function dragEnter() {
+  this.classList.add("over");
+}
+
+function dragLeave() {
+  this.classList.remove("over");
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragDrop() {
+  const dragEndIndex = +this.getAttribute("data-index");
+  swapItems(dragStartIndex, dragEndIndex);
+
+  this.classList.remove("over");
+}
+
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector(".draggable");
+  const itemTwo = listItems[toIndex].querySelector(".draggable");
+
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+}
+
+function addEventListeners() {
+  const draggables = document.querySelectorAll(".draggable");
+  const dragListItems = document.querySelectorAll(".draggable-list li");
+
+  draggables.forEach((draggable) => {
+    draggable.addEventListener("dragstart", dragStart);
+  });
+
+  dragListItems.forEach((item) => {
+    item.addEventListener("dragover", dragOver);
+    item.addEventListener("drop", dragDrop);
+    item.addEventListener("dragenter", dragEnter);
+    item.addEventListener("dragleave", dragLeave);
+  });
 }
